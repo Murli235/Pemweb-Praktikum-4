a766eb4f0795720549e618aa9c5cfa9c9f2e9d18
Murli235 <104454351+Murli235@users.noreply.github.com>
2022-12-12 15:28:04 +0700

    Add files via upload

---
+++ Prak 4.js
@@ -0,0 +1,6 @@
+function gantitema() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  document.querySelector(".left").classList.toggle("dark-mode");
+  document.querySelector(".right").classList.toggle("dark-mode");
+}
